31855ef0f514cd2347b35f52217f7fb1d208e743
Johan Park <parkjohan@gmail.com>
2019-05-03 18:42:35 -0700

    added UIProgressView added UIToolbarItems refactored code

--- WKWebView Project/ViewController.swift
+++ WKWebView Project/ViewController.swift
@@ -11,51 +11,95 @@ import UIKit
 //import WebKit
 import WebKit
 
+//conform to WKNavigationDelegate. It has only optional protocols
 class ViewController: UIViewController, WKNavigationDelegate {
     
-    //create a property of type WKWebView!
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    
+    var websites = ["apple.com", "google.com"]
     
     //loadView() func
     override func loadView() {
+        
         //create an instance of WKWebView()
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let url = URL(string: "https://www.apple.com") else { return }
+        //get URL and make a load URLRequest
+        guard let url = URL(string: "https://www." + websites[0]) else { return }
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(openTapped))
-
+        //set BarButtonItem on the top right of page
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        //add UIToolbar items with UIBarButtonItems
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let reload = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        //add progressView
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        //add items to toolbarItems array
+        toolbarItems = [progressButton, spacer, reload]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     }
     
     //create actionSheet UIAlertActionController for bar button drop down
     @objc func openTapped() {
-        let alert = UIAlertController(title: "Open", message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Google.com", style: .default, handler: openPage))
-        alert.addAction(UIAlertAction(title: "Apple.com", style: .default, handler: openPage))
+        let alert = UIAlertController(title: "Open new page!", message: nil, preferredStyle: .actionSheet)
+        
+        for website in websites {
+            alert.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        alert.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        alert.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
         present(alert, animated: true)
     }
     
     //handlers for each UIAlertAction
     func openPage(action: UIAlertAction) {
         guard let actionTitle = action.title else { return }
-        guard let url = URL(string: "https://+\(actionTitle)") else { return }
+        guard let url = URL(string: "https://" + actionTitle) else { return }
         webView.load(URLRequest(url: url))
     }
     
+    //set webView title to web page title
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
     }
-
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
 }
 
